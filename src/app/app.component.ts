39833a1f1b4ea45a07bc892131b8374a9172b9a4
saurabh9495 <saurabhkumar4829@gmail.com>
2018-08-18 21:12:26 +0530

    REST API CALL
    adding of api,backend functionality
    
    Signed-off-by: saurabh9495 <saurabhkumar4829@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'state';
+  private status:any;
+  private ok:any;
+
+  constructor(private http: HttpClient) { }
+ 
+  ngOnInit() {
+    return this.http.get('https://mongojiva.test.openebs.io/api/pod/status')
+    .subscribe(data =>{
+      console.log(data)
+      this.ok= data;
+      this.status = this.ok.status;
+      console.log(this.ok.status)
+    });
+
+
+  }
 }
