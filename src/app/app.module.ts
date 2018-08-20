e89d6fa1cc5009970210719777901a4923b0f3f4
inyee786 <intakhab.cusat@gmail.com>
2018-08-20 19:44:45 +0530

    add footer and header same as openebs website
    this contain footer component and header component
    
    Signed-off-by: inyee786 <intakhab.cusat@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     HttpClientModule,
